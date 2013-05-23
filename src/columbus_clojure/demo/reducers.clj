83292a9927d003de78cee01c3b946ed2785da395
Ryan McGowan <ryan@ryanmcg.com>
2013-05-23 00:27:39 -0400

    Define sum-reducer example.

--- src/columbus_clojure/demo/reducers.clj
+++ src/columbus_clojure/demo/reducers.clj
@@ -1,6 +1,27 @@
-(ns columbus-clojure.demo.reducers)
+(ns columbus-clojure.demo.reducers
+  (:require [clojure.core.reducers :as r]
+            [criterium.core :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def big-number 10000000)
+
+; There might be a better way but this works
+(def big-lazy-range (lazy-seq (into [] (range big-number))))
+(def big-non-lazy-range (into [] (range big-number)))
+
+(defn sum-reducer [memo v]
+  ;(Thread/sleep 10)
+  (+ memo v))
+
+(defn core-sum-reducer [coll]
+  (reduce sum-reducer coll))
+
+(defn reducer-sum-reducer [coll]
+  (r/reduce sum-reducer 0 coll))
+
+(defn -main
+  "Just a main function"
+  []
+  (doseq [reduce-version [reducer-sum-reducer core-sum-reducer]
+          coll [big-lazy-range big-non-lazy-range]]
+    (with-progress-reporting
+      (quick-bench (core-sum-reducer coll)))))
