0574ee0fde03fb5bb887d06a45cd6cf8a9ae98f7
Ryan McGowan <ryan@ryanmcg.com>
2013-05-29 01:30:55 -0400

    Test to ensure that the sum core and reducer versions produce the same result.

--- test/columbus_clojure/demo/reducers_test.clj
+++ test/columbus_clojure/demo/reducers_test.clj
@@ -2,6 +2,32 @@
   (:require [clojure.test :refer :all]
             [columbus-clojure.demo.reducers :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def small-number 1000)
+
+; There might be a better way but this works
+(def small-lazy-range (lazy-seq (into [] (range (inc small-number)))))
+(def small-non-lazy-range (into [] (range (inc small-number))))
+
+(defn gaussian-sum
+  "Figure out what the sum should be."
+  [nu]
+  (-> nu
+      (+ 1)
+      (* nu)
+      (/ 2)))
+
+(deftest sum-reducer-versions-work
+  (let [output (gaussian-sum small-number)]
+    (are [sum-reducer coll] (= output (sum-reducer coll))
+         reducer-sum-reducer small-non-lazy-range
+         reducer-sum-reducer small-lazy-range
+         core-sum-reducer small-non-lazy-range
+         core-sum-reducer small-lazy-range)))
+
+(deftest multi-reducer-versions-work
+  (let [output (gaussian-sum small-number)]
+    (are [sum-reducer coll] (= output (sum-reducer coll))
+         reducer-sum-reducer small-non-lazy-range
+         reducer-sum-reducer small-lazy-range
+         core-sum-reducer small-non-lazy-range
+         core-sum-reducer small-lazy-range)))
