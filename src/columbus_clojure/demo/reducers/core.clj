1ce0c7bf972818e43a1ea19b547e89b678702a56
Ryan McGowan <ryan@ryanmcg.com>
2013-05-31 22:11:12 -0400

    Implement histogram output function.

--- src/columbus_clojure/demo/reducers/core.clj
+++ src/columbus_clojure/demo/reducers/core.clj
@@ -2,26 +2,30 @@
   (:require [clojure.core.reducers :as r]
             [clojure.pprint :as pp]
             [foldable-seq.core :refer [foldable-seq]]
-            (columbus-clojure.demo.reducers [benchmark :refer [mean-bench]])))
+            (incanter [charts :as ic]
+                      [core :as in])
+            (columbus-clojure.demo.reducers
+              [benchmark :as bench])))
 
 (def big-number 10000)
 (def multiplier-count 100)
 
 ; There might be a better way but this works
 (def big-lazy-range
-  "A lazy version of a range to big-numer"
+  "Lazy Range"
   (lazy-seq (into [] (range big-number))))
+
 (def big-non-lazy-range
-  "A non-lazy version of a range to big-numer"
+  "Non-Lazy Range"
   (into [] (range big-number)))
 
 (defn core-sum-reducer
-  "C +"
+  "(+) Core"
   [coll]
   (reduce + coll))
 
 (defn reducer-sum-reducer
-  "R +"
+  "(+) Reducers"
   [coll]
   (r/reduce + coll))
 
@@ -32,51 +36,62 @@
            ~rng))
 
 (defn core-multi-reducer
-  "C *"
+  "(*) Core"
   [coll]
   (->> (range multiplier-count)
        (reduce-multiplier map coll)
        (reduce +)))
 
 (defn reducer-multi-reducer
-  "R *"
+  "(*) Reducers"
   [coll]
   (->> (range multiplier-count)
        (reduce-multiplier r/map coll)
        (r/fold +)))
 
-
 (defn fs-reducer-multi-reducer
-  "FS *"
+  "(*) Lazy Fold & Reducers"
   [coll]
   (->> (range multiplier-count)
        foldable-seq
        (reduce-multiplier r/map coll)
        (r/fold +)))
 
-(def historgram-output
-  "Create a histogram for eveything"
-  []
-  (doseq [reducer-version [#'reducer-sum-reducer
-                           #'core-sum-reducer
-                           #'fs-reducer-multi-reducer
-                           #'reducer-multi-reducer
-                           #'core-multi-reducer]
-          coll [big-lazy-range lazy-range]]
+(defn historgram-output
+  "Create a histogram for the given symbols (should be tied to functions). If no
+  symbols are given then use all of them"
+  [& reducer-version-symbols]
+  (doseq [reducer-version (if (empty? reducer-version-symbols)
+                            [#'reducer-sum-reducer
+                             #'core-sum-reducer
+                             #'fs-reducer-multi-reducer
+                             #'reducer-multi-reducer
+                             #'core-multi-reducer]
+                            reducer-version-symbols)
+          coll [#'big-lazy-range #'big-non-lazy-range]]
     (let [reducer-doc (:doc (meta reducer-version))
           coll-doc (:doc (meta coll))]
-      )))
+      (-> (reducer-version (var-get coll))
+          (bench/benchmark-times)
+          (bench/data->times)
+          (ic/histogram :nbins 5
+                        :title (str reducer-doc " with a " coll-doc)
+                        :x-label "Execution Time (ms)")
+          (in/view)))))
+
 
 (defn table-output
   "Output results as a table"
-  []
+  [& reducer-version-symbols]
   (pp/print-table
     [:reducer :lazy :non-lazy]
-    (for [reducer-version [#'reducer-sum-reducer
-                           #'core-sum-reducer
-                           #'fs-reducer-multi-reducer
-                           #'reducer-multi-reducer
-                           #'core-multi-reducer]]
+    (for [reducer-version (if (empty? reducer-version-symbols)
+                            [#'reducer-sum-reducer
+                             #'core-sum-reducer
+                             #'fs-reducer-multi-reducer
+                             #'reducer-multi-reducer
+                             #'core-multi-reducer]
+                            reducer-version-symbols)]
       {:reducer (:doc (meta reducer-version))
-       :lazy (mean-bench (reducer-version big-lazy-range))
-       :non-lazy (mean-bench (reducer-version big-non-lazy-range))})))
+       :lazy (bench/mean-bench (reducer-version big-lazy-range))
+       :non-lazy (bench/mean-bench (reducer-version big-non-lazy-range))})))
