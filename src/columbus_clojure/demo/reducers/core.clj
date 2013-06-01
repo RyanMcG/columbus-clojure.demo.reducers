da1df61b9a3183387cd7732b6c56fdca3c77672a
Ryan McGowan <ryan@ryanmcg.com>
2013-05-31 20:48:31 -0400

    DRY it up and spilt into two output methods.

--- src/columbus_clojure/demo/reducers/core.clj
+++ src/columbus_clojure/demo/reducers/core.clj
@@ -5,10 +5,15 @@
             (columbus-clojure.demo.reducers [benchmark :refer [mean-bench]])))
 
 (def big-number 10000)
+(def multiplier-count 100)
 
 ; There might be a better way but this works
-(def big-lazy-range (lazy-seq (into [] (range big-number))))
-(def big-non-lazy-range (into [] (range big-number)))
+(def big-lazy-range
+  "A lazy version of a range to big-numer"
+  (lazy-seq (into [] (range big-number))))
+(def big-non-lazy-range
+  "A non-lazy version of a range to big-numer"
+  (into [] (range big-number)))
 
 (defn core-sum-reducer
   "C +"
@@ -20,36 +25,50 @@
   [coll]
   (r/reduce + coll))
 
+(defmacro reduce-multiplier [map-version coll rng]
+  `(reduce (fn [reducible# multiplier#]
+             (~map-version (partial * (mod multiplier# 5)) reducible#))
+           ~coll
+           ~rng))
+
 (defn core-multi-reducer
   "C *"
   [coll]
-  (->> (range 100)
-       (reduce (fn [reducible multiplier]
-                 (map (partial * (mod multiplier 5)) reducible))
-               coll)
+  (->> (range multiplier-count)
+       (reduce-multiplier map coll)
        (reduce +)))
 
 (defn reducer-multi-reducer
   "R *"
   [coll]
-  (->> (range 100)
-       (reduce (fn [reducible multiplier]
-                 (r/map (partial * (mod multiplier 5)) reducible))
-               coll)
+  (->> (range multiplier-count)
+       (reduce-multiplier r/map coll)
        (r/fold +)))
+
 
 (defn fs-reducer-multi-reducer
   "FS *"
   [coll]
-  (->> (range 100)
+  (->> (range multiplier-count)
        foldable-seq
-       (reduce (fn [reducible multiplier]
-                 (r/map (partial * (mod multiplier 5)) reducible))
-               coll)
+       (reduce-multiplier r/map coll)
        (r/fold +)))
 
-(defn -main
-  "Just a main function"
+(def historgram-output
+  "Create a histogram for eveything"
+  []
+  (doseq [reducer-version [#'reducer-sum-reducer
+                           #'core-sum-reducer
+                           #'fs-reducer-multi-reducer
+                           #'reducer-multi-reducer
+                           #'core-multi-reducer]
+          coll [big-lazy-range lazy-range]]
+    (let [reducer-doc (:doc (meta reducer-version))
+          coll-doc (:doc (meta coll))]
+      )))
+
+(defn table-output
+  "Output results as a table"
   []
   (pp/print-table
     [:reducer :lazy :non-lazy]
