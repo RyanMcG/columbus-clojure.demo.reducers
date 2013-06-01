352a905592bdf64aa757efa5c66ed2b624b5b260
Ryan McGowan <ryan@ryanmcg.com>
2013-05-31 22:10:53 -0400

    Add data->times func to benchmark ns.

--- src/columbus_clojure/demo/reducers/benchmark.clj
+++ src/columbus_clojure/demo/reducers/benchmark.clj
@@ -10,13 +10,25 @@
       samples warmup-jit-period target-execution-time f opts overhead)))
 
 (defmacro quick-benchmark-times
-  [expr options]
-  `(benchmark-times* (fn [] ~expr) (merge *default-quick-bench-opts* ~options)))
+  [expr & [options]]
+  `(benchmark-times* (fn [] ~expr) (merge *default-quick-bench-opts*
+                                          (or ~options {}))))
+
+(defmacro benchmark-times
+  [expr & [options]]
+  `(benchmark-times* (fn [] ~expr) (or ~options {})))
 
 (defmacro mean-bench
   "A mean benching macro"
   [expr]
-  `(-> ~expr
-       (quick-benchmark {})
-       :mean
-       first))
+  `(let [result# (quick-benchmark ~expr {})]
+     (println (report-result result#))
+     (-> result#
+         :mean
+         first)))
+
+(defn data->times
+  [{:keys [samples execution-count]}]
+  (let [scale (/ 1e-6 execution-count)]
+    (map (fn [datum] (* scale (double datum)))
+         samples)))
